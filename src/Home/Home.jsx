a75e218762176fd59b7ca2eeea3a5318844df2a6
O-keita <o.keita@alustudent.com>
2023-10-19 05:15:10 +0200

    first commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+function Home() {
+  return (
+    <div>
+      <div className="background"></div>
+      <h1><span>Hi</span><br/><span> I'am Omar Keita</span><br /><span>Software Developer</span></h1>
+       
+
+       
+    </div>  
+ 
+    )
+}
+
+export default Home
