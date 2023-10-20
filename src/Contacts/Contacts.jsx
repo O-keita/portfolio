83b3d670e82d5ffcd2952494e7e9bbd1d5e43ff0
O-keita <o.keita@alustudent.com>
2023-10-20 20:48:23 +0200

    footer modified

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import "./contacts.css"
 import {BiLogoLinkedin} from "react-icons/bi"
 import {FaFacebookF} from "react-icons/fa"
+import {BiLogoGmail} from "react-icons/bi"
 
 const Contacts = () => {
+
+    const date = new Date()
+    const year =date.getFullYear()
 
   
 
@@ -15,9 +19,11 @@ const Contacts = () => {
 
             <div className="icons"><a href="https://www.linkedin.com/in/omar-keita-a055a8237/" target='_blank' rel="noreferrer"><BiLogoLinkedin className='linkedin-icon'/></a></div>
             <div className="icons"><a href="https://www.facebook.com/profile.php?id=100092564769034" target='_blank' rel="noreferrer"><FaFacebookF className='facebook-icon '/></a></div>
+            <div className="icons"> <BiLogoGmail/> <span>omarmusakeita@gmail.com</span></div>
             
 
         </div>
+        <div className="footer"><span>&copy;{year} Omar Keita</span></div>
         
             
             
