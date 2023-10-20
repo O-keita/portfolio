c07d997fa1c04e8f80eae8d629e568e1da57098d
O-keita <o.keita@alustudent.com>
2023-10-20 16:05:31 +0200

    skills component ready

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import "./skills.css"
 import SkillsCard from './SkillsCard'
 import {BiLogoReact} from "react-icons/bi"
+import {SiPython} from "react-icons/si"
+import {IoLogoJavascript} from "react-icons/io"
+import {FcLinux} from "react-icons/fc"
+import {FcAcceptDatabase} from "react-icons/fc"
+import {ImHtmlFive} from "react-icons/im"
 
 const Skills = () => {
   return (
@@ -11,10 +16,47 @@ const Skills = () => {
 
           <SkillsCard
 
-          icon =<BiLogoReact/>
+          icon =<BiLogoReact className='react'/>
           name = "ReactJS"
           
           />
+            <SkillsCard
+
+          icon =<IoLogoJavascript className='javascript-icon'/>
+          name = "Javascript"
+
+          />
+
+           <SkillsCard
+
+          icon =<SiPython className='python-icon'/>
+          name = "Python"
+
+          />
+          <SkillsCard
+
+            icon =<FcLinux/>
+            name = "Linux"
+
+          />
+
+          <SkillsCard
+
+          icon =<FcAcceptDatabase/>
+          name = "Database Management"
+
+          />
+          <SkillsCard
+
+          icon =<ImHtmlFive className='html-icon'/>
+          name = "HTML & CSS"
+
+          />
+
+          
+
+    
+       
 
         </div>
 
