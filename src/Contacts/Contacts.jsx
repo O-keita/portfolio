24115308337179a00d19f917356dd90a6a20164d
O-keita <o.keita@alustudent.com>
2023-10-30 13:55:26 +0200

    Cv download now available

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -3,6 +3,8 @@ import "./contacts.css"
 import {BiLogoLinkedin} from "react-icons/bi"
 import {FaFacebookF} from "react-icons/fa"
 import {BiLogoGmail} from "react-icons/bi"
+import {AiOutlineDownload} from "react-icons/ai"
+import CV from "./FinalCV.pdf"
 
 const Contacts = () => {
 
@@ -12,23 +14,28 @@ const Contacts = () => {
   
 
   return (
-    <div className='contacts' id="contact">
+    <div className="footer">
+         <div className="title"><span>Contact me</span></div>
+      <div className='contacts' id="contact">
 
-        <div className="title"><span>Contact me</span></div>
-        <div className="socials">
+        
+          <div className="socials">
 
-            <div className="icons"><a href="https://www.linkedin.com/in/omar-keita-a055a8237/" target='_blank' rel="noreferrer"><BiLogoLinkedin className='linkedin-icon'/></a></div>
-            <div className="icons"><a href="https://www.facebook.com/profile.php?id=100092564769034" target='_blank' rel="noreferrer"><FaFacebookF className='facebook-icon '/></a></div>
-            <div className="icons"> <BiLogoGmail/> <span>omarmusakeita@gmail.com</span></div>
-            
+              <div className="icons"><a href="https://www.linkedin.com/in/omar-keita-a055a8237/" target='_blank' rel="noreferrer"><BiLogoLinkedin className='linkedin-icon'/></a></div>
+              <div className="icons"><a href="https://www.facebook.com/profile.php?id=100092564769034" target='_blank' rel="noreferrer"><FaFacebookF className='facebook-icon '/></a></div>
+              <div className="icons"> <BiLogoGmail/> <span>omarmusakeita@gmail.com</span></div>
+              
 
+          </div>
+          <div className="cv"><a href={CV} download target='_blank' rel='noreferrer'><button>Download CV <AiOutlineDownload className='download-icon'/></button></a></div>
+          
+          
+              
+              
+          
+          </div>
+          <div className="footer"><span>&copy;{year} Omar Keita</span></div>
         </div>
-        <div className="footer"><span>&copy;{year} Omar Keita</span></div>
-        
-            
-            
-        
-    </div>
   )
 }
 
